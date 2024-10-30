1446080179e5054222d1b109e543aba7c240ef0b
ljbarzol <ljbarzol@espol.edu.ec>
2024-10-30 09:41:50 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+ const databaseURL = 'https://landing.firebaseio.com/data.json'; 
+
+ let sendData = () => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+ fetch(databaseURL, {
+         method: 'POST', // Método de la solicitud
+         headers: {
+             'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+         },
+         body: JSON.stringify(data) // Convierte los datos a JSON
+     })
+     .then(response => {
+         if (!response.ok) {
+             throw new Error(`Error en la solicitud: ${response.statusText}`);
+         }
+         return response.json(); // Procesa la respuesta como JSON
+     })
+     .then(result => {
+         alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+         form.reset()
+     })
+     .catch(error => {
+         alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+     });
+
+ }
+
+
 let ready = () => {
     console.log('DOM está listo')
     debugger
@@ -15,7 +46,8 @@ let loaded = ( eventLoaded ) => {
         const emailText = emailElement.value;
 
         if (emailText.length === 0) {
-          emailElement.focus()
+          emailElement.focus();
+            return;
         }
     })
 
